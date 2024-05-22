314451abb40ac27c951c56e2f0cf1edb6333de4a
BMRG-Holgat <117574552+BMRG-Holgat@users.noreply.github.com>
2024-05-22 10:09:17 +0100

    update to mySignals
    updated signals definitions

--- mySignalAliases.h
+++ mySignalAliases.h
@@ -8,19 +8,19 @@
 ALIAS(SIG_A1, 318) // Track A board 1
 ALIAS(SIG_B1, 321) // Track B board 1
 ALIAS(SIG_D1, 324) // Track D board 1
-ALIAS(SIG_H1, 468) // Holgate Boad 3
-ALIAS(SIG_A2, 504) // Track A board 4
-ALIAS(SIG_B2, 507) // Track B board 4
-ALIAS(SIG_C2, 510) // Track C board 4
-ALIAS(SIG_D2, 513) // Track D board 4
-ALIAS(SIG_E2, 516) // Track E board 4
-ALIAS(SIG_F2, 519) // Track F board 4
-ALIAS(SIG_A3, 714) // Track A board 7
-ALIAS(SIG_B3, 687) // Track B board 7
-ALIAS(SIG_D3, 731) // Track C board 7
-ALIAS(SIG_C3, 834) // Track c board 9
-ALIAS(SIG_E3, 831) // Track E board 9
-ALIAS(SIG_F3, 828) // Track F board 9
+ALIAS(SIG_H1, 182) // Holgate Boad 3
+ALIAS(SIG_A2, 264) // Track A board 4
+ALIAS(SIG_B2, 268) // Track B board 4
+ALIAS(SIG_C2, 272) // Track C board 4
+ALIAS(SIG_D2, 276) // Track D board 4
+ALIAS(SIG_E2, 188) // Track E board 4
+ALIAS(SIG_F2, 192) // Track F board 4
+ALIAS(SIG_A3, 280) // Track A board 7
+ALIAS(SIG_B3, 284) // Track B board 7
+ALIAS(SIG_D3, 288) // Track C board 7
+ALIAS(SIG_C3, 262) // Track c board 9
+ALIAS(SIG_E3, 259) // Track E board 9
+ALIAS(SIG_F3, 256) // Track F board 9
 /*
 ALIAS(SIG_N1_E, 182) //sig bypass 2 through signal
 ALIAS(SIG_N1_F, 185) //bypass 1 approach
--- mySignals.h
+++ mySignals.h
@@ -6,20 +6,19 @@
 * SIGNALH(red,amber(0),green)
 */
 //Define signals 4 i/o devices needed
-//SIGNALH(440, 0, 441) // Board 4 Holgate
-SIGNALH(504, 505, 506) // Board 4 track A
-SIGNALH(507, 508, 509) // Board 4track B
-SIGNALH(510, 511, 512) // Board 4 track C
-SIGNALH(513, 514, 515) // Board 4 track D
-SIGNALH(516, 517, 518) // Board 4 track E
-SIGNALH(519, 520, 521) // Board 4 track F
+SIGNALH(182, 0, 183) // Board 3  Holgate Exit
+SIGNALH(264, 265, 266) // Board 4 track A
+SIGNALH(268, 269, 270) // Board 4track B
+SIGNALH(272, 273, 274) // Board 4 track C
+SIGNALH(276, 277, 278) // Board 4 track D
+SIGNALH(188, 189, 190) // Board 4 track E
+SIGNALH(192, 193, 194) // Board 4 track F
 SIGNALH(318, 319, 320) // Board 1
 SIGNALH(321, 322, 323) // Board 1
 SIGNALH(324, 325, 326) // Board 1 
-SIGNALH(468,0,469) // Board 3 sidings
-SIGNALH(714, 715, 716) // Board 7 -- change when added
-SIGNALH(687, 688, 689) // Board 7
-SIGNALH(731, 732, 733) // Board 7 
-SIGNALH(834, 0, 835) // Board 9   C
-SIGNALH(828, 829, 830) // Board 9   F
-SIGNALH(831, 832, 833) // Board 9   E
+SIGNALH(280, 281, 282) // Board 7 -- change when added
+SIGNALH(284, 285, 286) // Board 7
+SIGNALH(288, 289, 290) // Board 7 
+SIGNALH(262, 0, 263) // Board 9   C
+SIGNALH(259, 260, 261) // Board 9   F
+SIGNALH(256, 257, 258) // Board 9   E
