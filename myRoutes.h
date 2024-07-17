a4780870086ea222d485d6849f0bc837d1367c5b
BMRG-Holgat <holgate@funkyredapples.com>
2024-07-17 09:34:51 +0100

    Update myRoutes.h
    added sound files

--- myRoutes.h
+++ myRoutes.h
@@ -3,18 +3,24 @@
 * for churchbells
 */
 
-ROUTE(100,"Main Bells")
-    SET(1000)
+ROUTE(1000,"Main Bells")
+   /* PLAYSOUND(10000,0,0,DF_RESET)
+    DELAY(2000)
+    PLAYSOUND(10000,0,0,DF_FOLDER) // Set folder to 01 player 1 */
+    PLAYSOUND(10000,1,30,DF_PLAY)
 DONE
 
-ROUTE(101,"Wedding Peel")
-    SET(1001)
+ROUTE(1001,"Wedding Peel")
+    PLAYSOUND(10000,0,0,DF_RESET)
+    DELAY(2000)
+    PLAYSOUND(10000,0,02,DF_FOLDER) // Set folder to 01 player 1
+    PLAYSOUND(10000,002,30,DF_PLAY)
 DONE
 
-ROUTE(102,"Bells. THe bells")
-    SET(1002)
+ROUTE(1002,"Bells. THe bells")
+    SET(10002)
 DONE
 
-ROUTE(103,"More bells")
-    SET(1003)
+ROUTE(1003,"More bells")
+    SET(10003)
 DONE
